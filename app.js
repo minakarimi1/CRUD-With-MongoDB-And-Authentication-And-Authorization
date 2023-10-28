84b8ae4f2d08d35818cf22eb790d23fa7a5f857d
mohammad <m6336.programer@gmail.com>
2023-10-28 14:52:33 +0330

    added middlewares(helmet,morgan,body parser)

---
+++ app.js
@@ -0,0 +1,18 @@
+import 'dotenv/config'
+import express, { urlencoded } from 'express'
+import morgan from 'morgan';
+import helmet from 'helmet';
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.use(express.json()) //body parser
+app.use(express.urlencoded({ extended: true })); // parse data comes from HTML form
+app.use(helmet())
+app.use(morgan("tiny"))
+
+
+
+
+app.listen(port ,()=>{
+  console.log(`Run Server On Port: ${port}`);
+})
