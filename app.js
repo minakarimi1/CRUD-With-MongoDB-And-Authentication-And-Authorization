80713bb734f1d1963499379c077d369452efde13
minakarimi1 <minakarimi1@yahoo.com>
2023-10-30 18:30:52 +0330

    added homeRouter

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import express, { urlencoded } from 'express'
 import morgan from 'morgan';
 import helmet from 'helmet';
+import homeRouter from './routes/home-route';
 const app = express()
 const port = process.env.PORT || 3000;
 
@@ -9,6 +10,8 @@ app.use(express.json()) //body parser
 app.use(urlencoded({ extended: true })); // parse data comes from HTML form
 app.use(helmet())
 app.use(morgan("tiny"))
+
+app.use('/',homeRouter);
 
 
 
