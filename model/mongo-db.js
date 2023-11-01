8a881cd97283c3bca668ca50c6ded26d395175e8
mohammad <m6336.programer@gmail.com>
2023-11-01 15:38:45 +0330

    added getusers()

--- model/mongo-db.js
+++ model/mongo-db.js
@@ -1,25 +1,29 @@
 import mongoose from "mongoose";
-mongoose.connect('mongodb://127.0.0.1:27017/usearInfo')
-.then(()=>console.log("connect to database"))
-.catch((e)=>console.log("could not connect to database",e))
+mongoose
+  .connect("mongodb://127.0.0.1:27017/usearInfo")
+  .then(() => console.log("connect to database"))
+  .catch((e) => console.log("could not connect to database", e));
 
- 
-const userSchema = new mongoose.Schema ({
-  name: {type: String},
-  email: {type: String, require:true},
-  password: {type: String, require: true},
+const userSchema = new mongoose.Schema({
+  name: { type: String },
+  email: { type: String, require: true },
+  password: { type: String, require: true },
 });
-const User = mongoose.model('User',userSchema);
+const User = mongoose.model("User", userSchema);
 
-export async function insertUser(name , email , password){
-const user = new User ({
-  name,
-  email,
-  password,
-})
+export async function insertUser(name, email, password) {
+  const user = new User({
+    name,
+    email,
+    password,
+  });
 
-const result = await user.save();
-console.log(result);
-return result;
+  const result = await user.save();
+  console.log(result);
+  return result;
+}
 
+export async function getusers() {
+  const users = await User.find();
+  return users
 }
