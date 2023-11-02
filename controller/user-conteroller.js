c16237ff20f72e7546403e95e3b3f3d310c26270
minakarimi1 <minakarimi1@yahoo.com>
2023-11-02 11:41:24 +0330

    added validate

--- controller/user-conteroller.js
+++ controller/user-conteroller.js
@@ -1,8 +1,33 @@
-import { insertUser,getUserEmail } from '../model/mongo-db.js';
-import 'dotenv/config'
+import { insertUser, getUserEmail } from "../model/mongo-db.js";
+import "dotenv/config";
+import Joi from "joi";
+import _ from "lodash";
+import bcrypt from "bcrypt";
 
 //register controler
 const register = async (req, res, next) => {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .max(50)
+      .required()
+      .messages({ "string.min": process.env.name_min || "" }),
+    email: Joi.string().email().required(),
+    password: Joi.string()
+      .min(6)
+      .max(100)
+      .required()
+      .messages({ "pass_min": process.env.pass_min || "" }),
+  };
+  //add object schema to JOi from up validate
+const validateResult = Joi.object(schema).validate(req.body);
+
+  //check DB for no repitly Email
+
+
+
+
+  // add to database
   const result = await insertUser(
     req.body.name,
     req.body.email,
@@ -15,4 +40,4 @@ const register = async (req, res, next) => {
     message: "ok",
   });
 };
-export default {register}
+export default { register };
