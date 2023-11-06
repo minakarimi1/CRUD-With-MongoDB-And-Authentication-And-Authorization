e0e3b81b80dcb006700b3cac17403862e1136959
minakarimi1 <minakarimi1@yahoo.com>
2023-11-06 17:42:34 +0330

    added auth to router

--- routes/user-router.js
+++ routes/user-router.js
@@ -1,8 +1,11 @@
 import userConteroller from "../controller/user-conteroller.js";
 import express from "express";
 const router = express.Router()
+import auth from '../middlewares/auth.js'
 
 
 router.post('/register',userConteroller.register)
+
+// router.use(auth)
 router.post('/login',userConteroller.login)
 export default router;
